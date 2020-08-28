4f341759189de229c7d0fa0b28739dbbbd24c172
C Shane Brown <cshanebrown78@gmail.com>
2020-08-28 15:32:51 -0500

    Coded initial redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <AppNavbar />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+          
+        </div>
+      </Provider>
+      
+    )
+  }  
+    ;
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <AppNavbar />
+//       <ShoppingList />
+//     </div>
+//   );
+// }
 
 export default App;
