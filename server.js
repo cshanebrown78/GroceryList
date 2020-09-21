f1c1af30bad90c4439cc1888b384c2881fa64893
C Shane Brown <cshanebrown78@gmail.com>
2020-09-21 10:30:01 -0500

    heroku work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3001;
 const path = require('path');
-const config = require('config');
+// const config = require('config');
 
 // const items = require('./routes/api/items')
 
